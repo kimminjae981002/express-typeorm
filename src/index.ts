a4df7093a4121e02f693721a638327fcbbd100fb
minjae Kim <cmg981548@gmail.com>
2024-04-07 21:26:23 +0900

    feat: [main] typeorm을 이용한 CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
 
 const app = express();
+
+const port = 3000;
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -18,7 +21,55 @@ AppDataSource.initialize()
   .catch((err) => {
     console.error(err);
   });
+// database 연결
 
-app.listen("3000", () => {
+app.post("/users", async (req, res) => {
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  // database에서 User 엔티티에 req.body내용을 생성하겠다.
+  const results = await AppDataSource.getRepository(User).save(user);
+  // database에서 User 엔티티에 user 내용을 저장하겠다.
+
+  return res.send(results);
+});
+
+app.get("/users", async (req, res) => {
+  const user = await AppDataSource.getRepository(User).find();
+
+  return res.send(user);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+
+  return res.send(user);
+});
+
+app.put("/users/:id", async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+
+  AppDataSource.getRepository(User).merge(user, req.body);
+  // 새로운 내용으로 merge를 해준다.
+  await AppDataSource.getRepository(User).save(user);
+  // 새로운 내용을 저장해준다.
+
+  return res.send(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+
+  await AppDataSource.getRepository(User).delete(user);
+  // 삭제.
+
+  return res.send("삭제 성공");
+});
+
+app.listen(port, () => {
   console.log("open server");
 });
